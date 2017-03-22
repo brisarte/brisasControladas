41ea0bbe12f606ca783146fcd17b8f47b323b23e
Evandro Carreira <evandro.carreira@gmail.com>
2017-03-22 11:22:37 -0300

    Estrutura de Brisas separada do main

--- src/ofApp.h
+++ src/ofApp.h
@@ -4,50 +4,8 @@
 #include "ofxOpenCv.h"
 #include "ofxKinect.h"
 #include "GuiApp.h"
+#include "Brisa.h"
 
-
-//classe da Brisa
-class Brisa {
-
-public:
-	bool ativa = false; // Controle de render
-	void setup();           	//Configura brisa
-	void update( float dt );   	//Recalcula brisa
-	void draw();
-
-	int tipoBrisa;
-
-	// Brisa Video
-	void setupVideo(string videoPath);
-	void updateKinect(ofxCvGrayscaleImage imgKinect);
-	string urlpath;
-	ofVideoPlayer video;
-	float hOriginal, wOriginal; //tamanhos do arquivo
-	float hVideo, wVideo; //tamanhos novos
-	ofFbo fboKinect;
-
-	// Brisa Contorno
-	ofxCvContourFinder contourFinder;
-
-	// Brisa Gif
-	void loadGif(string gifPath);
-	string urlpasta;
-	vector<ofTexture> listaImg;
-
-	// Brisa Poligono
-	void desenhaPoligono(int vertices, int radius, bool rotate, bool fill);
-	ofColor cor1,cor2;
-	int vertices;
-
-	// Brisa Illuminati e Girassol
-	ofPoint lookAt;
-	int whiteTotalSlow;
-	ofPoint baricentro;
-	ofPoint olhaPraMim();
-	ofImage olho, orbita, girassol;
-	ofxCvGrayscaleImage blurImage(ofxCvGrayscaleImage imgOriginal);
-
-};
 
 class ofApp : public ofBaseApp{
 
@@ -73,7 +31,6 @@ class ofApp : public ofBaseApp{
 		shared_ptr<ofAppBaseWindow> window;
 		shared_ptr<ofBaseGLRenderer> gl;
 
-		Brisa brisa1,brisa2,brisa3,brisa4;
     	bool ativaBrisa1,ativaBrisa2,ativaBrisa3,ativaBrisa4;
     	ofFbo fboLayer;
 };
